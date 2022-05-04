dcb94230836f2ba70d7cda238d06f539b847cf79
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-05-04 17:37:07 +0100

    Added local storage file and working through JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,113 @@ let questions = [
     answer: "all of the above",
   },
 ];
+
+function startButton() {
+  countdown = 100;
+  document.getElementById("countdown").innerHTML = countdown;
+
+  timer = setInterval(function () {
+    countdown--;
+    document.getElementById("countdown").innerHTML = countdown;
+    if (countdown <= 0) {
+      clearInterval(timer);
+      endGame();
+    }
+  }, 1000);
+
+  next();
+}
+
+function endGame() {
+  clearInterval(timer);
+
+  let quizContent =
+    ` <h2>Game over!</h2>
+<h3>You got a ` +
+    score +
+    ` /100!</h3>
+<h3>That means you got ` +
+    score / 20 +
+    ` questions correct!</h3>
+<input type="text" id="name" placeholder="Insert player name"> 
+<button onclick="setScore()">Set score!</button>`;
+
+  document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function setScore() {
+  localStorage.setItem("highscore", score);
+  localStorage.setItem("highscoreName", document.getElementById("name").value);
+  getScore();
+}
+
+function getScore() {
+  let quizContent =
+    `
+<h2>` +
+    localStorage.getItem("highscoreName") +
+    `'s highscore is:</h2>
+<h1>` +
+    localStorage.getItem("highscore") +
+    `</h1>
+<button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>`;
+  debugger;
+
+  document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function clearScore() {
+  localStorage.setItem("highscore", "");
+  localStorage.setItem("highscoreName", "");
+
+  resetGame();
+}
+
+function resetGame() {
+  clearInterval(timer);
+  score = 0;
+  currentQuestion = -1;
+  countdown = 0;
+  timer = null;
+
+  document.getElementById("countdown").innerHTML = countdown;
+
+  document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function incorrect() {
+  countdown -= 15;
+  next();
+}
+
+function correct() {
+  score += 20;
+  next();
+}
+
+function next() {
+  currentQuestion++;
+
+  if (currentQuestion > questions.length - 1) {
+    endGame();
+    return;
+  }
+
+  let quizContent = "<h2>" + questions[currentQuestion].title + "</h2>";
+
+  for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+    let btn = '<button onclick="[ANS]">[CHOICE]</button>';
+    btn = btn.replace("[CHOICE]", questions[currentQuestion].choices[i]);
+
+    if (
+      questions[currentQuestion].choices[i] == questions[currentQuestion].answer
+    ) {
+      btn = btn.replace("[ANS]", "correct()");
+    } else {
+      btn = btn.replace("[ANS]", "incorrect()");
+    }
+    quizContent += btn;
+  }
+
+  document.getElementById("quizBody").innerHTML = quizContent;
+}
